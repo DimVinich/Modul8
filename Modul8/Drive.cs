a64f5904d34b6f3b13837569cdc924c73158441d
DimVinich <DmitrijVS@gmail.com>
2022-06-19 11:29:55 +0300

    Добавьте файлы проекта.

---
+++ Modul8/Drive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul8
+{
+    public class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public long TotalSpace { get; }
+        public long FreeSpace { get; }
+
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+    }
+
+
+}
